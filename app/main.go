95389fc9ce30ed50815106cdd82c069917c57877
3cham <thanhtung.nov@gmail.com>
2020-11-26 11:04:39 +0100

    add post for update player score

--- app/main.go
+++ app/main.go
@@ -1,28 +1,60 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
 )
 
+var (
+	ErrPlayerNotFound = errors.New("Player not found")
+)
+
 type PlayerStore interface {
-	GetPlayerScore(name string) int
+	GetPlayerScore(name string) (int, error)
+	UpdatePlayerScore(name string)
 }
 
 type PlayerServer struct {
 	store PlayerStore
 }
 
+type InMemoryStore struct {}
+func (s *InMemoryStore) GetPlayerScore(name string) (int, error) {
+	return 123, nil
+}
+
+func (s *InMemoryStore) UpdatePlayerScore(name string) {
+}
+
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	score := p.store.GetPlayerScore(player)
-	fmt.Fprint(w, score)
+	switch r.Method {
+	case http.MethodGet:
+		p.showScore(w, player)
+	case http.MethodPost:
+		p.updateScore(w, player)
+	}
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+	score, err := p.store.GetPlayerScore(player)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		fmt.Fprint(w, score)
+	}
+}
+
+func (p *PlayerServer) updateScore(w http.ResponseWriter, player string) {
+	p.store.UpdatePlayerScore(player)
+	w.WriteHeader(http.StatusAccepted)
 }
 
 func main() {
-	server := &PlayerServer{}
+	server := &PlayerServer{&InMemoryStore{}}
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
--- app/main_test.go
+++ app/main_test.go
@@ -4,23 +4,58 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
 type StubPlayerStore struct {
 	scores map[string]int
+	updateScoreCalls []string
 }
 
-func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	return s.scores[name]
+func (s *StubPlayerStore) GetPlayerScore(name string) (int, error) {
+	val, found := s.scores[name]
+	if !found {
+		return 0, ErrPlayerNotFound
+	}
+	return val, nil
 }
+
+func (s *StubPlayerStore) UpdatePlayerScore(name string) {
+	s.updateScoreCalls = append(s.updateScoreCalls, name)
+}
+
+func assertRespondedScore(t *testing.T, response *httptest.ResponseRecorder, expected string) {
+	t.Helper()
+	got := response.Body.String()
+	if got != expected {
+		t.Fatalf("Wrong score for player returned, expected %s, got %s", expected, got)
+	}
+}
+
+func assertRespondedStatusCode(t *testing.T, response *httptest.ResponseRecorder, expected int) {
+	t.Helper()
+	got := response.Code
+	if got != expected {
+		t.Fatalf("Wrong status code returned, expected %d, got %d", expected, got)
+	}
+}
+
+func getScoreRequest(player string) (*http.Request, error) {
+	return http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", player), nil)
+}
+
+func postScoreRequest(player string, score int) (*http.Request, error) {
+	return http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", player), nil)
+}
+
 
 func TestGETPlayerScore(t *testing.T) {
 	scoresMap := map[string]int {
 		"A": 20,
 		"B": 10,
 	}
-	server := &PlayerServer{&StubPlayerStore{scoresMap}}
+	server := &PlayerServer{&StubPlayerStore{scoresMap, nil}}
 	t.Run("Return correct score for A", func(t *testing.T) {
 		request, _ := getScoreRequest("A")
 		response := httptest.NewRecorder()
@@ -36,16 +71,32 @@ func TestGETPlayerScore(t *testing.T) {
 		server.ServeHTTP(response, request)
 		assertRespondedScore(t, response, "10")
 	})
+
+	t.Run("Return 404 if player not found", func(t *testing.T) {
+		request, _ := getScoreRequest("C")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+		assertRespondedStatusCode(t, response, http.StatusNotFound)
+	})
 }
 
-func assertRespondedScore(t *testing.T, response *httptest.ResponseRecorder, expected string) {
-	t.Helper()
-	got := response.Body.String()
-	if got != expected {
-		t.Fatalf("Wrong score for player returned, expected %s, got %s", expected, got)
-	}
-}
+func TestUpdatePlayerStore(t *testing.T) {
 
-func getScoreRequest(player string) (*http.Request, error) {
-	return http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", player), nil)
+	store := StubPlayerStore{make(map[string]int), []string{}}
+	server := &PlayerServer{&store}
+
+	t.Run("POST score should return accepted", func(t *testing.T) {
+		request, _ := postScoreRequest("C", 10)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertRespondedStatusCode(t, response, http.StatusAccepted)
+
+		expectedScoreCalls := []string{"C"}
+		if !reflect.DeepEqual(store.updateScoreCalls, expectedScoreCalls) {
+			t.Fatalf("wrong update score calls, got %v, expected %v", store.updateScoreCalls, expectedScoreCalls)
+		}
+	})
 }

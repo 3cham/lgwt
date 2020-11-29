ffe0388a3a81a6ffe6506fe9f867a6d4e736951f
3cham <thanhtung.nov@gmail.com>
2020-11-29 11:44:29 +0100

    add sq into pipeline

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -1,11 +1,23 @@
 package pipeline
 
-func In(nums ...int) <-chan int {
+func gen(nums ...int) <-chan int {
 	out := make(chan int)
 
 	go func() {
 		for _, num := range (nums) {
 			out <- num
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(input <-chan int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		for num := range input {
+			out <- num * num
 		}
 		close(out)
 	}()
--- pipeline/pipeline_test.go
+++ pipeline/pipeline_test.go
@@ -1,16 +1,42 @@
 package pipeline
 
-import "testing"
+import (
+	"reflect"
+	"sort"
+	"testing"
+)
 
-func TestIn(t *testing.T) {
-	t.Run("In() should produce all the number in the parameters", func(t *testing.T) {
-		out := In(2,3,4,5)
+func makeSortedArrFromChan(c <-chan int) []int {
+	result := []int{}
+	for num := range c {
+		result = append(result, num)
+	}
+	sort.Ints(result)
+	return result
+}
+
+func TestGen(t *testing.T) {
+	t.Run("gen() should produce all the number in the parameters", func(t *testing.T) {
+		out := gen(2, 3, 4, 5)
 		count := 0
 		for _ = range out {
-			count ++
+			count++
 		}
 		if count != 4 {
-			t.Fatalf("In() does not return as expected, expected 4 numbers, got %d", count)
+			t.Fatalf("gen() does not return as expected, expected 4 numbers, got %d", count)
+		}
+	})
+}
+
+func TestSq(t *testing.T) {
+	t.Run("sq() should return squared numbers", func(t *testing.T) {
+		in := gen(1, 2)
+		out := sq(in)
+
+		sortedOut := makeSortedArrFromChan(out)
+		var exected = makeSortedArrFromChan(gen(1, 4))
+		if !reflect.DeepEqual(sortedOut, exected) {
+			t.Fatalf("gen() does not return expected results, expected %v, got %v", exected, sortedOut)
 		}
 	})
 }

f6abf359574452763d7a14ec12e153618a285c45
3cham <thanhtung.nov@gmail.com>
2021-02-04 23:43:20 +0100

    skip test Program with interrupt signal

--- context/prog_test.go
+++ context/prog_test.go
@@ -4,5 +4,6 @@ import "testing"
 
 // This calls the Program and we could cancelled it with ctrl-C and still invoke the cleaning event
 func TestProgram(t *testing.T) {
+	t.Skip("skipping because interrupt signal is not sent by intellij")
 	Program()
 }
